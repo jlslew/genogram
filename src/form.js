02b592f4383d4e7d5233701e7e55db7efd4e15d1
Jonathan Lee Slew <jonathan.leeslew@hotmail.com>
2017-03-27 21:31:49 +0400

    Integrate firebase into the project

--- src/form.js
+++ src/form.js
@@ -1,4 +1,4 @@
-module.exports = function(ko) {
+module.exports = function(ko, firebase) {
     return function(array) {
         function Form() {
             var self = this;
@@ -20,6 +20,7 @@ module.exports = function(ko) {
                 });
             });
 
+            self.id = ko.observable('');
             self.key = ko.observable('');
             self.first = ko.observable('');
             self.last = ko.observable('');
@@ -31,12 +32,13 @@ module.exports = function(ko) {
             self.dead = ko.observable(false);
             self.dod = ko.observable('');
 
-            self.key.subscribe(function(value) {
+            self.id.subscribe(function(value) {
                 if (value === undefined) {
                     self.reset();
                 } else {
                     for (var i = 0; i < self.persons().length; i++) {
                         if (self.persons()[i].id === value) {
+                            self.key(self.persons()[i].key);
                             self.first(self.persons()[i].fn);
                             self.last(self.persons()[i].ln);
                             self.sex(self.persons()[i].s);
@@ -52,6 +54,7 @@ module.exports = function(ko) {
             });
 
             self.reset = function() {
+                self.id('');
                 self.key('');
                 self.first('');
                 self.last('');
@@ -62,6 +65,48 @@ module.exports = function(ko) {
                 self.dob('');
                 self.dead(false);
                 self.dod('');
+            };
+
+            self.save = function(form) {
+                var person = {};
+                person['key'] = self.key();
+                person['fn'] = self.first();
+                person['ln'] = self.last();
+                person['s'] = self.sex();
+
+                if (self.mother() !== undefined && self.father() !== undefined) {
+                    person['m'] = self.mother();
+                    person['f'] = self.father();
+                }
+
+                if (self.spouse() !== undefined) {
+                    person['p'] = self.spouse();
+                }
+
+                person['b'] = jQuery(form).find('#input-birth').val();
+                person['d'] = jQuery(form).find('#input-death').val();
+
+                if (self.dead() && person['d'] === '') {
+                    person['d'] = ' ';
+                }
+            };
+
+            self.push = function(person) {
+                var id = self.id();
+
+                if (id === undefined || id === '') {
+                    id = firebase.database().ref(self.db()).push().key;
+                    person['key'] = (new Date()).getTime();
+                } else {
+                    for (var i = 0; i < self.persons().length; i++) {
+                        if (self.persons()[i].key === person['key']) {
+                            self.persons.replace(self.persons()[i], person);
+                        }
+                    }
+                }
+
+                firebase.database().ref(self.db() + '/' + id).set(person);
+                self.reset();
             };
         }
 
